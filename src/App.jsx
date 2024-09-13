a9aa73c06396adf96681a4a61f305d087e3c6c11
sanan-sajid <sanansajid015@gmail.com>
2024-09-14 01:01:05 +0530

    lol

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
+import img1 from "./assets/img1.png";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
 function Header() {
+  let temp = genRandomInt(2);
   return (
     <>
       <h1>This is an header</h1>
-      <h2>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat
-        provident expedita ipsa optio? Repellat repellendus, reiciendis neque,
-        doloribus laborum laboriosam hic distinctio a tempore officia eaque quam
-        necessitatibus praesentium dolor.
-      </h2>
+      <p>The random number is {temp}</p>
+      <h2>{reactDescriptions[temp]}</h2>
+      {/* <img src={img1}></img> */}
+      {/* <img src="/img1.png" alt="Image" /> */}
     </>
   );
 }
 function App() {
   return (
     <>
-      <Header></Header>
+      <Header />
     </>
   );
 }
