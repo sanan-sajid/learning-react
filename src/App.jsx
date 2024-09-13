3740792176e96eb2bb68c68d60a8a8518e754db2
sanan-sajid <sanansajid015@gmail.com>
2024-09-14 01:04:47 +0530

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import img1 from "./assets/img1.png";
-
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
+
+function CoreConcept(props) {
+  return (
+    <>
+      <h1>Your Name is {props.name}. </h1>
+      <h2>and your age is {props.age}.</h2>
+    </>
+  );
+}
+
 function Header() {
   let temp = genRandomInt(2);
   return (
@@ -13,7 +22,7 @@ function Header() {
       <p>The random number is {temp}</p>
       <h2>{reactDescriptions[temp]}</h2>
       {/* <img src={img1}></img> */}
-      {/* <img src="/img1.png" alt="Image" /> */}
+      <CoreConcept name="sanan" age="21" />
     </>
   );
 }
