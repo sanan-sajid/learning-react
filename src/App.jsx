ad3183186ef556c9a61116c8ef754e3bdc9121da
sanan-sajid <sanansajid015@gmail.com>
2024-09-14 00:42:23 +0530

    custom components

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,23 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
-
-function App() {
-  // const [count, setCount] = useState(0)
-
+function Header() {
   return (
     <>
-      <h1>test</h1>
-      <h2>yo</h2>
+      <h1>This is an header</h1>
+      <h2>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat
+        provident expedita ipsa optio? Repellat repellendus, reiciendis neque,
+        doloribus laborum laboriosam hic distinctio a tempore officia eaque quam
+        necessitatibus praesentium dolor.
+      </h2>
+    </>
+  );
+}
+function App() {
+  return (
+    <>
+      <Header></Header>
     </>
   );
 }
