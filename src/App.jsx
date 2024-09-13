a4e13d9db994724a6f23b1875096723b1b766b51
sanan-sajid <sanansajid015@gmail.com>
2024-09-14 01:23:28 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,5 @@
-import img1 from "./assets/img1.png";
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+import Header from "./components/Header.jsx";
 
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function CoreConcept(props) {
-  return (
-    <>
-      <h1>Your Name is {props.name}. </h1>
-      <h2>and your age is {props.age}.</h2>
-    </>
-  );
-}
-
-function Header() {
-  let temp = genRandomInt(2);
-  return (
-    <>
-      <h1>This is an header</h1>
-      <p>The random number is {temp}</p>
-      <h2>{reactDescriptions[temp]}</h2>
-      {/* <img src={img1}></img> */}
-      <CoreConcept name="sanan" age="21" />
-    </>
-  );
-}
 function App() {
   return (
     <>
---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+// import img1 from "./assets/img1.png";
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function CoreConcept(props) {
+  return (
+    <>
+      <h1>Your Name is {props.name}. </h1>
+      <h2>and your age is {props.age}.</h2>
+    </>
+  );
+}
+export default function Header() {
+  let temp = genRandomInt(2);
+  return (
+    <>
+      <h1>This is an header</h1>
+      <p>The random number is {temp}</p>
+      <h2>{reactDescriptions[temp]}</h2>
+      {/* <img src={img1}></img> */}
+      <CoreConcept name="sanan" age="21" />
+    </>
+  );
+}
